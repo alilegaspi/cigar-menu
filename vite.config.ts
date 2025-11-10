571246716a81fc83cffc2f2f0696ea64cc227a9a
alilegaspi <alilegaspi@gmail.com>
2025-11-10 20:32:59 +0800

    Update GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/cigar-menu/',
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    outDir: 'dist'
   }
 });
