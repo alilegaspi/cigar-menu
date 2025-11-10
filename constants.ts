ab8e9438023a0ac77ed7ac4cdd3b38cbdd9d33ce
alilegaspi <alilegaspi@gmail.com>
2025-11-11 02:20:19 +0800

    fix: add leading slash to Antonio Gimenez image paths

--- constants.ts
+++ constants.ts
@@ -3,8 +3,8 @@ export const CIGARS_DATA: Cigar[] = [
   {
     id: 1,
     name: 'ANTONIO GIMENEZ CHURCHILL',
-    image: 'images/ANTONIO GIMENEZ CHURCHILL.png',
-    detailsImage: 'images/ANTONIO GIMENEZ CHURCHILL details.png',
+    image: '/images/ANTONIO GIMENEZ CHURCHILL.png',
+    detailsImage: '/images/ANTONIO GIMENEZ CHURCHILL details.png',
     price: '₱1,200.00',
     origin: 'Philippines',
     profile: 'Mild - Medium',
